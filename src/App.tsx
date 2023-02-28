4f5a0319d7ab8b14f332a4d94e4fa88cf58a7673
LeviVitez <bmwyolo555@gmail.com>
2023-02-28 08:53:54 +0100

    asdaf

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 class App extends React.Component {
 
